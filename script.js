0e2aef381b2223b163544152f7bfc0399d9d7f8b
Kenaii008 <al45778@un.edu.mx>
2025-12-02 10:31:04 -0600

    7

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ card.addEventListener('click', function() {
     if (masculino) {
         nombre.textContent = 'Sad Boyzz';
         ocupacion.textContent = 'Cantante triste';
+        masculino = false;
     } else {
         nombre.textContent = 'Junior H';
         ocupacion.textContent = 'Cantante de corridos';
+        masculino = true;
     }
     alert ('Perfil sad boyz cambiado');
-    masculino = masculino;
+    //masculino = masculino;
 });
