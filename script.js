f11e7f1245854233086005c5d62138c7b5fce1a4
Kenaii008 <al45778@un.edu.mx>
2025-12-02 10:10:44 -0600

    5

---
+++ script.js
@@ -0,0 +1,6 @@
+var card = document.querySelector('.perfil-card');
+var nombre = card.querySelector('h2');
+var ocupacion = card.querySelector('.ocupacion');
+var masculino = true;
+
+card.addEventListener('click', function() 
